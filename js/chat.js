4f34d033e0ba398b0e0dc6bf1bd020a6c42a95e0
Intelisite <intelisite.io@gmail.com>
2025-05-10 20:34:19 -0300

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -127,7 +127,7 @@ fetch("https://web-production-e8469.up.railway.app/validar-chave", {
           messages: historico,
           model: "gpt-3.5-turbo",
           openai_key: assistenteIA.openaiKey,
-          plugin_key: assistenteIA.,
+		  plugin_key: assistenteIA.pluginKey,
 		  nicho: assistenteIA.nicho
         })
       });
